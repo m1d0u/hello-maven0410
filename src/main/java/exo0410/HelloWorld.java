c1a0c495c80794983c5c70fe42a9122e3b723f83
Imad <tab@mail.team.com>
2022-10-04 10:52:53 +0200

    changement

--- src/main/java/exo0410/HelloWorld.java
+++ src/main/java/exo0410/HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         
-        System.out.println("Hello Maven") ;	
+        System.out.println("Hello Maven 2") ;	
     }
 }
