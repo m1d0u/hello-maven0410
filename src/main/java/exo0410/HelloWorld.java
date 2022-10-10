4797369badb1f798421bd23fc484c2160e391629
m1d0u <114923534+m1d0u@users.noreply.github.com>
2022-10-10 10:26:04 +0200

    v3 pour check jenkins

--- src/main/java/exo0410/HelloWorld.java
+++ src/main/java/exo0410/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         
-       String message = "hello world 2";
+       String message = "hello world 3";
        afficher a = new afficher();
        a.display(message);
     	
